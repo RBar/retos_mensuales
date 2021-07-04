2d81ea5f4607fdde9df5fa8aaa860879e75e58d2
RoqueAsap <roque@asap507.com>
2021-07-04 12:15:56 -0500

    clase 2

--- retos/urls.py
+++ retos/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.index),
     path("<int:mes>", views.retos_mensuales_por_numero),
-    path("<str:mes>", views.retos_mensuales)
+    path("<str:mes>", views.retos_mensuales, name="retos-mensuales")
 ]
